d343d41d6d4940317ee2904759dabf8bde50d939
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-03-28 11:34:50 -0700

    add getAllSmoothies() method to api/read

--- src/api/read.ts
+++ src/api/read.ts
@@ -1,7 +1,13 @@
+import { Smoothie } from "../models/Smoothie"
+import { Ingredient } from "../models/Ingredient"
+
+import {smoothies} from '../data/smoothies.json'
 // GET SMOOTHIE
 
 // GET ALL SMOOTHIES
-
+export async function getAllSmoothies(): Promise<Smoothie[]> {
+    return smoothies as Smoothie[]
+}
 // GET INGREDIENT
 
 // GET ALL INGREDIENTS
