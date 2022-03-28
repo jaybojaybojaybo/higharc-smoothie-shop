7cc87ebb16fec6bc8316b38cce8751d3ac0b8308
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-03-28 11:37:10 -0700

    call getAllSmoothies() and create p with template iterator for returned smoothies

--- src/components/SmoothieList.vue
+++ src/components/SmoothieList.vue
@@ -1,3 +1,24 @@
 <template>
     <p class="bg-blue-600 text-white">The list of smoothies</p>
+    <p v-for="(smoothie, index) in smoothies">{{ smoothie.name }}</p>
 </template>
+
+<script lang="ts">
+import { onMounted, ref } from "vue"
+import { Smoothie } from "../models/Smoothie"
+
+import {getAllSmoothies} from '../api/read'
+
+export default {
+    setup() {
+        let smoothies = ref<Smoothie[]>()
+        onMounted(async () => {
+            console.log('hey what')
+            smoothies.value = await getAllSmoothies() as Smoothie[]
+        })
+        return {
+            smoothies
+        }
+    }
+}
+</script>
