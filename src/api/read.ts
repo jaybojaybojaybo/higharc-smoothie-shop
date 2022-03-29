33b6f25e39a8e710dfc9edd07191c3fcb57e7e9e
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-03-28 17:54:04 -0700

    write getSmoothie and getIngredient methods

--- src/api/read.ts
+++ src/api/read.ts
@@ -3,13 +3,20 @@ import Ingredient from "../models/Ingredient"
 
 import {smoothies} from '../data/smoothies.json'
 import {ingredients} from '../data/ingredients.json'
+
 // GET SMOOTHIE
+export async function getSmoothie(id: number): Promise<Smoothie> {
+    return smoothies.find((s) => s.id === id) as Smoothie
+}
 
 // GET ALL SMOOTHIES
 export async function getAllSmoothies(): Promise<Smoothie[]> {
     return smoothies as Smoothie[]
 }
 // GET INGREDIENT
+export async function getIngredient(id: number): Promise<Ingredient> {
+    return ingredients.find((i) => i.id === id) as Ingredient
+}
 
 // GET ALL INGREDIENTS
 export async function getAllIngredients(): Promise<Ingredient[]> {
