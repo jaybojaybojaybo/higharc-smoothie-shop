e366bdde89d0664da4ea24309ec9771313db2e1c
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-03-28 17:31:26 -0700

    import SmoothieCard. update Smoothie interface import

--- src/components/SmoothieList.vue
+++ src/components/SmoothieList.vue
@@ -1,15 +1,19 @@
 <template>
     <p class="bg-blue-600 text-white">The list of smoothies</p>
-    <p v-for="(smoothie, index) of smoothiesList" :key="index">{{ smoothie.name }}</p>
+    <SmoothieCard v-for="(smoothie, index) of smoothiesList" :key="index" :smoothie="smoothie"></SmoothieCard>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue"
-import { Smoothie } from "../models/Smoothie"
 
+import Smoothie from "../models/Smoothie"
+import SmoothieCard from "./SmoothieCard.vue"
 import { getAllSmoothies } from '../api/read'
 
 export default defineComponent({
+    components: {
+        SmoothieCard
+    },
     setup() {
         let smoothiesList = ref<Smoothie[]>()
         onMounted(async () => {
