b7f7a1ef98061b542fea356493b7d8f5138e3038
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-03-29 23:58:30 -0700

    remove id property to save time on writing id handlers for new smoothies

--- src/models/Ingredient.ts
+++ src/models/Ingredient.ts
@@ -1,7 +1,6 @@
 import {IngredientType} from './IngredientType'
 
 export default interface Ingredient extends Object{
-    id: number,
     name: string,
     type: IngredientType
 }
--- src/models/Smoothie.ts
+++ src/models/Smoothie.ts
@@ -1,7 +1,6 @@
 import Ingredient from "./Ingredient";
 
 export default interface Smoothie extends Object{
-    id: number,
     name: string,
     ingredients: Array<Ingredient>
 }
