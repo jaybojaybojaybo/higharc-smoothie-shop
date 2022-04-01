678fed8521acb6c0db7166c28ad5e42f04e3d9f0
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-04-01 04:44:34 -0700

    begrudgingly add hard-coded parent attributes to accurately target the gltf Group

--- src/composables/useIntersectHandler.ts
+++ src/composables/useIntersectHandler.ts
@@ -1,26 +1,29 @@
 import { Ref } from 'vue'
-import { Scene, Vector3, Intersection, Mesh, InstancedMesh } from 'three'
+import { Scene, Vector3, Intersection, Mesh, InstancedMesh, Group } from 'three'
 import Smoothie from '../models/Smoothie'
 import { CSS3DSprite } from 'three/examples/jsm/renderers/CSS3DRenderer'
 
-export default function useIntersectHandler(event: MouseEvent, raycasterRef: Ref, webGLScene: Scene, smoothies: Ref<Smoothie[]>, selectedSmoothie: Ref<Smoothie>, clearSmoothieSelection: VoidFunction) {  
+export default function useIntersectHandler(event: MouseEvent, raycasterRef: Ref, webGLScene: Scene, smoothies: Ref<Smoothie[]>, selectedSmoothies: Ref<Smoothie[]>, clearSmoothieSelection: Function) {  
   event.preventDefault()
   // Find intersections.
   let intersects = raycasterRef.value.intersectObjects(webGLScene.children, true) as Intersection[]
+  let addMode = true
   if (intersects.length > 0) {
     for (const intersect of intersects) {
-      if (intersect.object instanceof InstancedMesh) {
-        console.log('this the one! ', intersect)
-        if (selectedSmoothie.value) {
-          clearSmoothieSelection()
+      let targetParent = intersect.object.parent?.parent?.parent?.parent?.parent
+      if (targetParent && targetParent instanceof Group) {
+        if (selectedSmoothies.value.length) {
+          clearSmoothieSelection(!addMode)
         }
-        //@ts-ignore
-        selectedSmoothie.value = smoothies.value[intersect.instanceId as number]
-        console.log('SELECTENING: ', selectedSmoothie.value)
+        selectedSmoothies.value?.push(smoothies.value[parseInt(targetParent.name)])
         break
-      } 
-    intersects = []
+      } else {
+        clearSmoothieSelection(addMode)
+      }
+      intersects = []
     }
+  } else {
+    clearSmoothieSelection(addMode)
   }
 
 }
