08850938ec07c796a5973199bbf5829db61767ba
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-04-01 04:41:55 -0700

    add custom blue color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'smoothie-blue': '#00bae9',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
