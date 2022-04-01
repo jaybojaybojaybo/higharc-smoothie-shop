bf7bb2ba8fbd66e3ec5e3dbff96e5489da68b127
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-03-31 17:01:17 -0700

    remove code meant for SmoothieDetails

--- src/components/AddSmoothieForm.vue
+++ src/components/AddSmoothieForm.vue
@@ -11,16 +11,9 @@ import Ingredient from "../models/Ingredient";
 import ingredients from '../data/ingredients.json'
 import { getAllIngredients } from "../api/read";
 
-export default defineComponent({
-    props: {
-        smoothie: {
-          type: Object as PropType<Smoothie>,
-          required: false,
-        },
-    },
+export default defineComponent({    
     emits: ['addSmoothie', 'deleteSmoothie'],
     setup(props, { emit }) {
-        const currentSmoothie = toRef(props, 'smoothie')
         const css3dScene = inject("css3dScene") as Scene;
         const ingredientOptions = ref([] as Ingredient[])
         const contentString = ref("")
@@ -82,45 +75,6 @@ export default defineComponent({
             css3dScene.add(css.value);
         }
 
-        async function detailsFormInit() {
-            let ingredientsContent = ref("")
-            for (let iO of (currentSmoothie.value as Smoothie).ingredients) {
-                let ingredientNameHtml = String.raw
-                    `<div class="w-full px-3">                        
-                        <span class="form-label inline-block ml-2">${iO.name}</span>
-                    </div>`
-                ingredientsContent.value += ingredientNameHtml
-            }
-            // CREATE DETAILS HTML
-            contentString.value = 
-                `<h1 class="text-2xl font-bold text-white">${currentSmoothie.value?.name}</h1>
-                <div class="justify-center flex text-white">` +
-                    ingredientsContent.value
-                + `</div>`
-            // PREP FORM FOR CSS3D
-            const currentSmoothieContent = new DOMParser().parseFromString(
-                contentString.value,
-                "text/html"
-            );
-            currentSmoothieContent.body.style.background = new Color(0x000000).getStyle();
-            // CREATE CSS3D
-            css.value = new CSS3DSprite(currentSmoothieContent.body);
-            css.value.position.copy(new Vector3(x, y, z));
-            const scaleFactor = .007
-            css.value.scale.set(scaleFactor, scaleFactor, scaleFactor);
-            // LISTENERS
-            css.value.element.addEventListener('mousedown', handleClick, true)
-            // ADD TO SCENE
-            css3dScene.add(css.value);
-        }
-
-        watch(
-            () => (currentSmoothie.value as Smoothie), 
-            (newValue: Smoothie) => {
-                console.log('smoothie update in Readout!')
-            }
-        )
-
         onMounted(async() => {            
             addFormInit()
         })
@@ -178,7 +132,6 @@ export default defineComponent({
         return {
             handleClick,
             addFormInit,
-            detailsFormInit
         }
     },
 });
